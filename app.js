09626e4fc2da0d015daf6f184486ec5edb44ee41
CesarAcosta1 <65782056+CesarAcosta1@users.noreply.github.com>
2022-12-02 11:50:51 -0300

    test2

--- app.js
+++ app.js
@@ -1,41 +1,6 @@
-const server = require('http').createServer()
-const io = require('socket.io')(server)
-
-io.on('connection', function (client) {
-
-  console.log('client connect...', client.id);
-
-  client.on('typing', function name(data) {
-    console.log(data);
-    io.emit('typing', data)
-  })
-
-  client.on('message', function name(data) {
-    console.log(data);
-    io.emit('message', data)
-  })
-
-  client.on('location', function name(data) {
-    console.log(data);
-    io.emit('location', data);
-  })
-
-  client.on('connect', function () {
-  })
-
-  client.on('disconnect', function () {
-    console.log('client disconnect...', client.id)
-    // handleDisconnect()
-  })
-
-  client.on('error', function (err) {
-    console.log('received error from client:', client.id)
-    console.log(err)
-  })
+const app = require('express')()
+const http = require('http').createServer(app)
+app.get('/', (req, res) => {
+   res.send("Node Server is running. Yay!!")
 })
-
-var server_port = process.env.PORT || 3000;
-server.listen(server_port, function (err) {
-  if (err) throw err
-  console.log('Listening on port %d', server_port);
-});
+http.listen(8080)
