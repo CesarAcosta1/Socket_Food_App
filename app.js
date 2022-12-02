f6fe429f461d9a5462c28bce1c1b2ef7872d1ebc
CesarAcosta1 <65782056+CesarAcosta1@users.noreply.github.com>
2022-12-02 14:17:58 -0300

    Test

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-app.get('/', (req, res) => {
-   res.send("Node Server is running. Yay!!")
-})
-http.listen(8080)
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+
+const server = express()
+  .use((req, res) => res.send("Funciona"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
